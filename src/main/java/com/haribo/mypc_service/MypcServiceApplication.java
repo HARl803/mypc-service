cae3d206a064bfa429470c6b00a7593e62ed9514
unknown <wsup97@gmail.com>
2024-07-22 10:12:12 +0900

    Initial commit

---
+++ src/main/java/com/haribo/mypc_service/MypcServiceApplication.java
@@ -0,0 +1,13 @@
+package com.haribo.mypc_service;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MypcServiceApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(MypcServiceApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/haribo/mypc_service/MypcServiceApplicationTests.java
@@ -0,0 +1,13 @@
+package com.haribo.mypc_service;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class MypcServiceApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
