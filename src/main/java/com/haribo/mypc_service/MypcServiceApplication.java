3bf0d6c05f1f3b8e45e3544d6bb6d4e80440a93e
unknown <wsup97@gmail.com>
2024-07-24 13:43:35 +0900

    ✨ feat: MyComputerDto 추가 MyComputerDto 코드 작성까지

--- src/main/java/com/haribo/mypc_service/MypcServiceApplication.java
+++ src/main/java/com/haribo/mypc_service/MypcServiceApplication.java
@@ -2,12 +2,11 @@ package com.haribo.mypc_service;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.data.mongodb.repository.config.EnableMongoRepositories;
 
 @SpringBootApplication
 public class MypcServiceApplication {
-
 	public static void main(String[] args) {
 		SpringApplication.run(MypcServiceApplication.class, args);
 	}
-
 }
---
+++ src/main/java/com/haribo/mypc_service/application/dto/MyComputerDto.java
@@ -0,0 +1,37 @@
+package com.haribo.mypc_service.application.dto;
+
+import jakarta.persistence.Id;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+@Document(collection = "mycomputer")
+@Getter
+@Setter
+@NoArgsConstructor
+@AllArgsConstructor
+public class MyComputerDto {
+
+    @Id
+    private String id;
+    private String userId;
+    private MyComputer myComputer;
+
+    @NoArgsConstructor
+    @AllArgsConstructor
+    @Getter
+    public static class MyComputer {
+        private String computerName;
+        private String cpuId;
+        private String coolerId;
+        private String motherboardId;
+        private String graphicsCardId;
+        private String ssdId;
+        private String hddId;
+        private String pcCaseId;
+        private String poserSupplyId;
+        private String memoryId;
+    }
+}
