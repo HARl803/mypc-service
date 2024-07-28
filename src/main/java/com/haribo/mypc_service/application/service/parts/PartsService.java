68baa79f24c323ff107d0e616f2fa405b496ac30
unknown <wsup97@gmail.com>
2024-07-28 19:20:48 +0900

    ✨ feat[#4]: parts관련 Service 로직 구현 FE에서 비교 및 서칭 기능은 담당할 예정이기 때문에 Get 메서드 관련 로직만 필요

--- src/main/java/com/haribo/mypc_service/application/service/parts/PartsService.java
+++ src/main/java/com/haribo/mypc_service/application/service/parts/PartsService.java
@@ -1,5 +1,10 @@
 package com.haribo.mypc_service.application.service.parts;
 
 
+import com.haribo.mypc_service.application.dto.parts.*;
+
+import java.util.List;
+
 public interface PartsService {
+    <T> List<T> getAllParts(String partsName) ;
 }
--- src/main/java/com/haribo/mypc_service/application/service/parts/PartsServiceImpl.java
+++ src/main/java/com/haribo/mypc_service/application/service/parts/PartsServiceImpl.java
@@ -1,10 +1,35 @@
 package com.haribo.mypc_service.application.service.parts;
 
+import com.haribo.mypc_service.application.dto.parts.*;
 import lombok.RequiredArgsConstructor;
+import org.springframework.data.mongodb.core.MongoTemplate;
+import org.springframework.data.mongodb.core.query.Criteria;
+import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+
+import java.util.List;
 
 @RequiredArgsConstructor
 @Service
 public class PartsServiceImpl implements PartsService {
 
+    private final MongoTemplate mongoTemplate;
+
+    @Override
+    public <T> List<T> getAllParts(String partsName) {
+        Query query = new Query();
+        query.addCriteria(Criteria.where("partsName").is(partsName));
+
+        switch(partsName) {
+            case "cpu": return mongoTemplate.find(query, (Class<T>) CpuDto.class);
+            case "memory": return mongoTemplate.find(query, (Class<T>) MemoryDto.class);
+            case "case": return mongoTemplate.find(query, (Class<T>) CaseDto.class);
+            case "cooler": return mongoTemplate.find(query, (Class<T>) CoolerDto.class);
+            case "hdd": return mongoTemplate.find(query, (Class<T>) HddDto.class);
+            case "sdd": return mongoTemplate.find(query, (Class<T>) SddDto.class);
+            case "powersupply": return mongoTemplate.find(query, (Class<T>) PowerSupplyDto.class);
+            case "motherboard": return mongoTemplate.find(query, (Class<T>) MotherBoardDto.class);
+            default: return mongoTemplate.find(query, (Class<T>) GpuDto.class);
+        }
+    }
 }
