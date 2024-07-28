9446099e1df45158448fb09e6dd66460833882b4
unknown <wsup97@gmail.com>
2024-07-28 19:22:02 +0900

    ✨ feat[#4]: parts 컨트롤러 구현

--- src/main/java/com/haribo/mypc_service/presentation/AssemblePartsController.java
+++ src/main/java/com/haribo/mypc_service/presentation/AssemblePartsController.java
@@ -1,10 +1,25 @@
 package com.haribo.mypc_service.presentation;
 
-
+import com.haribo.mypc_service.application.service.parts.PartsService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
 
 @RestController
 @RequestMapping("/api/v1/parts")
 public class AssemblePartsController {
 
+    @Autowired
+    private PartsService partsService;
+
+    @GetMapping
+    @ResponseBody
+    private ResponseEntity<?> findParts(@RequestParam("partsName") String partsName){
+        List<Object> partsDtos = partsService.getAllParts(partsName);
+
+        return ResponseEntity.status(HttpStatus.OK).body(partsDtos);
+    }
 }
