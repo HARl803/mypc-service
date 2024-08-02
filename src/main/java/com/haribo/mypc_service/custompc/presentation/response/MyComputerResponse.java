6d480cb112e232956feb31b781430124d0335626
unknown <wsup97@gmail.com>
2024-08-02 13:38:57 +0900

    ✨ feat[#1]: response dto 생성

---
+++ src/main/java/com/haribo/mypc_service/custompc/presentation/response/MyComputerResponse.java
@@ -0,0 +1,28 @@
+package com.haribo.mypc_service.custompc.presentation.response;
+
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Getter;
+import com.haribo.mypc_service.custompc.application.dto.MyComputerDto.MyComputer.Parts;
+import org.springframework.data.mongodb.core.mapping.Field;
+
+
+@Getter
+@Builder
+@AllArgsConstructor
+public class MyComputerResponse {
+    private String computerName;
+    private String id;
+    private Boolean isDelete;
+
+    private Parts cpu;
+    private Parts coolerTuning;
+    private Parts motherboard;
+    private Parts memory;
+    private Parts gpu;
+    private Parts ssd;
+    private Parts hdd;
+    @Field("case")
+    private Parts _case;
+    private Parts power;
+}
