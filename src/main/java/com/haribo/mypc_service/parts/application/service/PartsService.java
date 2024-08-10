e8c96c55286f28c140b5f112fc4b612ca6cb98e3
dwd9999 <ojw1010@gmail.com>
2024-08-11 02:02:05 +0900

    🎨 style: 불필요한 import 문 제거

--- src/main/java/com/haribo/mypc_service/parts/application/service/PartsService.java
+++ src/main/java/com/haribo/mypc_service/parts/application/service/PartsService.java
@@ -2,8 +2,6 @@ package com.haribo.mypc_service.parts.application.service;
 
 import com.haribo.mypc_service.parts.presentation.response.PartsResponse;
 
-import java.util.List;
-
 public interface PartsService {
     <T extends PartsResponse> T getAllParts(String partsName, Class<T> partsResponseType);
 }
