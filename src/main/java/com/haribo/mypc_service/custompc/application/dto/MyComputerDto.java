28ff95b573d34ea63e4db522915ed8470d52e0c4
sungup <wsup97@gmail.com>
2024-08-13 12:47:31 +0900

    ♻️ refactor[#1]: MyComputer를 기존 MyComputerDto 파일에서 분리

--- src/main/java/com/haribo/mypc_service/custompc/application/dto/MyComputerDto.java
+++ src/main/java/com/haribo/mypc_service/custompc/application/dto/MyComputerDto.java
@@ -1,5 +1,6 @@
 package com.haribo.mypc_service.custompc.application.dto;
 
+import jakarta.persistence.Id;
 import lombok.Builder;
 import lombok.Getter;
 import org.springframework.data.mongodb.core.mapping.Document;
@@ -11,26 +12,10 @@ import java.util.List;
 @Getter
 @Builder
 public class MyComputerDto {
+    @Id
+    private String id;
 
-    private String userId;
+    @Field("profile_id")
+    private String profileId;
     private List<MyComputer> myComputers;
-
-    @Getter
-    @Builder
-    public static class MyComputer {
-        private String id;
-        private String computerName;
-        private Boolean isDeleted;
-
-        private String cpu;
-        private String coolerTuning;
-        private String motherboard;
-        private String memory;
-        private String gpu;
-        private String ssd;
-        private String hdd;
-        @Field("case")
-        private String pcCase;
-        private String power;
-    }
 }
