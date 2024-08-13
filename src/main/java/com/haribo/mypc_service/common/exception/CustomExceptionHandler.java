c1f5267fe7bacd43fa7c34b97d29b47eb31e5fe8
sungup <wsup97@gmail.com>
2024-08-13 12:45:01 +0900

    🐛 fix[#1]: 예외 처리 handler 수정, 기존의 상태 코드 반환하지 않던 문제 해결

--- src/main/java/com/haribo/mypc_service/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/haribo/mypc_service/common/exception/CustomExceptionHandler.java
@@ -2,6 +2,8 @@ package com.haribo.mypc_service.common.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
 
@@ -10,13 +12,13 @@ import org.springframework.web.bind.annotation.RestControllerAdvice;
 public class CustomExceptionHandler {
 
     @ExceptionHandler(CustomException.class)
-    public CustomErrorResponse handleCustomException(
+    public ResponseEntity<?> handleCustomException(
             CustomException e,
             HttpServletRequest request) {
-        log.error(request.getRequestURI(), e.getStatusMessage());
-        return CustomErrorResponse.builder()
+        log.error("에러!! 요청 URL: {}, 에러 메시지: {}", request.getRequestURI(), e.getStatusMessage());
+        return new ResponseEntity<>(CustomErrorResponse.builder()
                 .status(e.getCustomErrorCode())
                 .statusMessage(e.getStatusMessage())
-                .build();
+                .build(), e.getCustomErrorCode().getStatus());
     }
 }
