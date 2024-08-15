6185743ba3b15372f33f39691497d7861afcd0e7
sungup <wsup97@gmail.com>
2024-08-16 05:53:19 +0900

    ✨ feat: 가장 상위 json 디렉토리에 부품명칭 추가해서 응답 보내기

--- src/main/java/com/haribo/mypc_service/parts/application/service/PartsServiceImpl.java
+++ src/main/java/com/haribo/mypc_service/parts/application/service/PartsServiceImpl.java
@@ -2,6 +2,7 @@ package com.haribo.mypc_service.parts.application.service;
 
 import com.haribo.mypc_service.parts.presentation.response.*;
 import lombok.RequiredArgsConstructor;
+import org.bson.json.JsonObject;
 import org.json.JSONObject;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
@@ -18,7 +19,7 @@ public class PartsServiceImpl implements PartsService {
     @Override
     public Map<?, ?> getParts() {
 
-        JSONObject response = new JSONObject();
+        JSONObject responses = new JSONObject();
 
         CpuResponse cpuResponse = getAllParts("cpu", CpuResponse.class);
         MemoryResponse memoryResponse = getAllParts("memory", MemoryResponse.class);
@@ -30,35 +31,53 @@ public class PartsServiceImpl implements PartsService {
         MotherboardResponse motherboardResponse =  getAllParts("motherboard", MotherboardResponse.class);
         GpuResponse gpuResponse = getAllParts("gpu", GpuResponse.class);
 
+        JSONObject response = new JSONObject();
         for (Map.Entry<String, CpuResponse.Cpu> entry : cpuResponse.getCpu().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("cpu", response);
+        response = new JSONObject();
         for (Map.Entry<String, MemoryResponse.Memory> entry : memoryResponse.getMemory().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("memory", response);
+        response = new JSONObject();
         for (Map.Entry<String, CaseResponse.Case> entry : caseResponse.getPcCase().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("case", response);
+        response = new JSONObject();
         for (Map.Entry<String, CoolerResponse.Cooler> entry : coolerResponse.getCoolerTuning().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("coolerTuning", response);
+        response = new JSONObject();
         for (Map.Entry<String, HddResponse.Hdd> entry : hddResponse.getHdd().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("hdd", response);
+        response = new JSONObject();
         for (Map.Entry<String, SsdResponse.Ssd> entry : ssdResponse.getSsd().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("ssd", response);
+        response = new JSONObject();
         for (Map.Entry<String, PowerResponse.Power> entry : powerResponse.getPower().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("power", response);
+        response = new JSONObject();
         for (Map.Entry<String, MotherboardResponse.Motherboard> entry : motherboardResponse.getMotherboard().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("motherboard", response);
+        response = new JSONObject();
         for (Map.Entry<String, GpuResponse.Gpu> entry : gpuResponse.getGpu().entrySet()){
             response.put(entry.getKey(), entry.getValue());
         }
+        responses.put("gpu", response);
 
-        return response.toMap();
+        return responses.toMap();
     }
 
     @Override
