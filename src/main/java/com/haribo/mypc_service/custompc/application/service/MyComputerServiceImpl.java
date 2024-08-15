530b0a5a4358b079c46af145d1cb99a80e41a276
sungup <wsup97@gmail.com>
2024-08-15 23:53:22 +0900

    ✨ feat: 서비스 로직에 profileId 갖고 오는 부분 추가

--- src/main/java/com/haribo/mypc_service/custompc/application/service/MyComputerServiceImpl.java
+++ src/main/java/com/haribo/mypc_service/custompc/application/service/MyComputerServiceImpl.java
@@ -21,23 +21,18 @@ import org.springframework.web.client.RestTemplate;
 import java.net.URI;
 import java.util.*;
 
-@RequiredArgsConstructor
+
 @Service
 @Slf4j
+@RequiredArgsConstructor
 public class MyComputerServiceImpl implements MyComputerService {
 
     private final MongoTemplate mongoTemplate;
-    private final RestTemplate restTemplate;
-
-    @Value("${auth.url}")
-    private String AUTH_URL;
 
     @Override
     public int countMyComputer(String profileId) {
 
         log.info("내 컴퓨터 갯수 세기!");
-
-//        if(!checkProfile(profileId)) throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
 
         Query query = new Query();
         query.addCriteria(Criteria.where("profileId").is(profileId));
@@ -61,8 +56,6 @@ public class MyComputerServiceImpl implements MyComputerService {
 
     @Override
     public List<MyComputerResponse> getMyComputerList(String profileId) {
-
-        if(!checkProfile(profileId)) throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
 
         log.info("컴퓨터 리스트 모두 호출하기!");
 
@@ -103,8 +96,6 @@ public class MyComputerServiceImpl implements MyComputerService {
     public MyComputerResponse getMyComputerDto(String profileId, String computerId) {
 
         log.info("내가 등록한 컴퓨터 찾기!");
-
-        if(!checkProfile(profileId)) throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
 
         Query query = new Query();
         query.addCriteria(Criteria.where("profileId").is(profileId));
@@ -188,35 +179,43 @@ public class MyComputerServiceImpl implements MyComputerService {
 
         log.debug("request 컴퓨터 아이디: {}", computerId);
 
-//        if(!checkProfile(profileId)) throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
-
         // 여기서 내가 원하는 computer를 찾았어
         Query query = new Query();
         query.addCriteria(Criteria.where("myComputers._id").is(computerId));
+
+        MyComputerDto myComputerDto = mongoTemplate.findOne(query, MyComputerDto.class, "mycomputer");
+
+        log.info("찾은 도큐먼트 일치하는지 확인: {}, {}",
+                mongoTemplate.findOne(new Query().addCriteria(Criteria.where("profileId").is(profileId)), MyComputerDto.class, "mycomputer"),
+                mongoTemplate.findOne(query, MyComputerDto.class, "mycomputer").getProfileId()
+        );
 
         if(mongoTemplate.findOne(query, MyComputerDto.class)==null){
             throw new CustomException(CustomErrorCode.CUSTOM_PC_NOT_FOUND);
         }
 
-        MyComputer myComputer = MyComputer.builder()
-                .id(myComputerRequest.getId())
-                .computerName(myComputerRequest.getComputerName())
-                .cpu(myComputerRequest.getCpu())
-                .memory(myComputerRequest.getMemory())
-                .ssd(myComputerRequest.getSsd())
-                .hdd(myComputerRequest.getHdd())
-                .motherboard(myComputerRequest.getMotherboard())
-                .power(myComputerRequest.getPower())
-                .gpu(myComputerRequest.getGpu())
-                .coolerTuning(myComputerRequest.getCoolerTuning())
-                .pcCase(myComputerRequest.getPcCase())
-                .isDeleted(false)
-                .build();
+        if(myComputerDto.getProfileId().equals(profileId)) {
 
-        Update update = new Update();
-        update.set("myComputers.$", myComputer);
+            MyComputer myComputer = MyComputer.builder()
+                    .id(myComputerRequest.getId())
+                    .computerName(myComputerRequest.getComputerName())
+                    .cpu(myComputerRequest.getCpu())
+                    .memory(myComputerRequest.getMemory())
+                    .ssd(myComputerRequest.getSsd())
+                    .hdd(myComputerRequest.getHdd())
+                    .motherboard(myComputerRequest.getMotherboard())
+                    .power(myComputerRequest.getPower())
+                    .gpu(myComputerRequest.getGpu())
+                    .coolerTuning(myComputerRequest.getCoolerTuning())
+                    .pcCase(myComputerRequest.getPcCase())
+                    .isDeleted(false)
+                    .build();
 
-        mongoTemplate.updateFirst(query, update, MyComputerDto.class);
+            Update update = new Update();
+            update.set("myComputers.$", myComputer);
+
+            mongoTemplate.updateFirst(query, update, MyComputerDto.class);
+        }
     }
 
 
@@ -225,52 +224,28 @@ public class MyComputerServiceImpl implements MyComputerService {
 
         log.debug("해당 컴퓨터 삭제하기!");
 
-        MyComputerResponse myComputerDto = getMyComputerDto(profileId, computerId);
-
-        log.debug("computerDto의 이름 : {}", myComputerDto.getComputerName());
-
         Query query = new Query();
-
-        // if(!checkProfile(profileId)) throw new CustomException(CustomErrorCode.USER_NOT_FOUND);
-
         query.addCriteria(Criteria.where("myComputers._id").is(computerId));
-        if(mongoTemplate.findOne(query, MyComputerDto.class)==null){
+
+        if (mongoTemplate.findOne(query, MyComputerDto.class) == null) {
             throw new CustomException(CustomErrorCode.CUSTOM_PC_NOT_FOUND);
         }
 
-        if(mongoTemplate.findOne(new Query()
-                .addCriteria(Criteria.where("profileId").is(profileId)
-                .and("myComputers._id").is(computerId)
-                .and("isDeleted").is(true)), MyComputerDto.class)!=null){
-            throw new CustomException(CustomErrorCode.CUSTOM_PC_NOT_FOUND);
+        MyComputerDto myComputerDto = mongoTemplate.findOne(query, MyComputerDto.class, "mycomputer");
+
+        if(myComputerDto.getProfileId().equals(profileId)) {
+
+            if (mongoTemplate.findOne(new Query()
+                    .addCriteria(Criteria.where("profileId").is(profileId)
+                            .and("myComputers._id").is(computerId)
+                            .and("isDeleted").is(true)), MyComputerDto.class) != null) {
+                throw new CustomException(CustomErrorCode.CUSTOM_PC_NOT_FOUND);
+            }
+
+            Update update = new Update();
+            update.set("myComputers.$.isDeleted", true);
+
+            mongoTemplate.updateFirst(query, update, MyComputerDto.class);
         }
-
-        Update update = new Update();
-        update.set("myComputers.$.isDeleted", true);
-
-        mongoTemplate.updateFirst(query, update, MyComputerDto.class);
-    }
-
-    private Boolean checkProfile(String profileId) {
-
-        log.info("프로필 아이디 체크하기! : 아직 구현 안되어있어요... auth 어케 갔다와");
-
-        HttpHeaders headers = new HttpHeaders();
-        headers.put(HttpHeaders.ACCEPT, Collections.singletonList(MediaType.APPLICATION_JSON_VALUE));
-        HttpEntity<String> entity = new HttpEntity<>(headers);
-
-        URI authUrl = URI.create(AUTH_URL);
-
-        try {
-            ResponseEntity<?> map = restTemplate.exchange(authUrl, HttpMethod.GET, entity, LinkedHashMap.class);
-            log.debug("auth/profile 요청 시 오는 응답 BODY: {}", map.getBody());
-        } catch (Exception e) {
-            log.error(e.getMessage());
-        }
-
-//        return map.getBody().get("profileId")==profileId;
-
-        log.debug("일단 무조건 성공으로 반환. checkProfile 메서드 수정 필요");
-        return true;
     }
 }
