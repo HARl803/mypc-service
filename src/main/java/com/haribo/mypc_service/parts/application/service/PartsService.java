08af48b59422734d76766141efe34d6b59bb579e
sungup <wsup97@gmail.com>
2024-08-16 02:44:57 +0900

    ✨ feat: 종류 상관 없이 모든 부품을 한 번에 보내는 api를 위한 서비스 로직 작성

--- src/main/java/com/haribo/mypc_service/parts/application/service/PartsService.java
+++ src/main/java/com/haribo/mypc_service/parts/application/service/PartsService.java
@@ -1,7 +1,9 @@
 package com.haribo.mypc_service.parts.application.service;
 
 import com.haribo.mypc_service.parts.presentation.response.PartsResponse;
+import java.util.Map;
 
 public interface PartsService {
     <T extends PartsResponse> T getAllParts(String partsName, Class<T> partsResponseType);
+    Map<?, ?> getParts();
 }
--- src/main/java/com/haribo/mypc_service/parts/application/service/PartsServiceImpl.java
+++ src/main/java/com/haribo/mypc_service/parts/application/service/PartsServiceImpl.java
@@ -2,16 +2,64 @@ package com.haribo.mypc_service.parts.application.service;
 
 import com.haribo.mypc_service.parts.presentation.response.*;
 import lombok.RequiredArgsConstructor;
+import org.json.JSONObject;
 import org.springframework.data.mongodb.core.MongoTemplate;
 import org.springframework.data.mongodb.core.query.Criteria;
 import org.springframework.data.mongodb.core.query.Query;
 import org.springframework.stereotype.Service;
+import java.util.Map;
 
 @Service
 @RequiredArgsConstructor
 public class PartsServiceImpl implements PartsService {
 
     private final MongoTemplate mongoTemplate;
+
+    @Override
+    public Map<?, ?> getParts() {
+
+        JSONObject response = new JSONObject();
+
+        CpuResponse cpuResponse = getAllParts("cpu", CpuResponse.class);
+        MemoryResponse memoryResponse = getAllParts("memory", MemoryResponse.class);
+        CaseResponse caseResponse = getAllParts("case", CaseResponse.class);
+        CoolerResponse coolerResponse = getAllParts("coolerTuning", CoolerResponse.class);
+        HddResponse hddResponse = getAllParts("hdd", HddResponse.class);
+        SsdResponse ssdResponse = getAllParts("ssd", SsdResponse.class);
+        PowerResponse powerResponse = getAllParts("power", PowerResponse.class);
+        MotherboardResponse motherboardResponse =  getAllParts("motherboard", MotherboardResponse.class);
+        GpuResponse gpuResponse = getAllParts("gpu", GpuResponse.class);
+
+        for (Map.Entry<String, CpuResponse.Cpu> entry : cpuResponse.getCpu().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, MemoryResponse.Memory> entry : memoryResponse.getMemory().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, CaseResponse.Case> entry : caseResponse.getPcCase().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, CoolerResponse.Cooler> entry : coolerResponse.getCoolerTuning().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, HddResponse.Hdd> entry : hddResponse.getHdd().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, SsdResponse.Ssd> entry : ssdResponse.getSsd().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, PowerResponse.Power> entry : powerResponse.getPower().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, MotherboardResponse.Motherboard> entry : motherboardResponse.getMotherboard().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+        for (Map.Entry<String, GpuResponse.Gpu> entry : gpuResponse.getGpu().entrySet()){
+            response.put(entry.getKey(), entry.getValue());
+        }
+
+        return response.toMap();
+    }
 
     @Override
     public <T extends PartsResponse> T getAllParts(String partsName, Class<T> partType) {
