4f607675e4a68bc9a726b3f5f1cb322bd147a990
sungup <wsup97@gmail.com>
2024-08-15 23:49:50 +0900

    ✨ feat: custom에러 헨들러 작성

--- src/main/java/com/haribo/mypc_service/common/exception/CustomExceptionHandler.java
+++ src/main/java/com/haribo/mypc_service/common/exception/CustomExceptionHandler.java
@@ -2,10 +2,10 @@ package com.haribo.mypc_service.common.exception;
 
 import jakarta.servlet.http.HttpServletRequest;
 import lombok.extern.slf4j.Slf4j;
-import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.RestControllerAdvice;
+import org.springframework.web.client.HttpClientErrorException;
 
 @Slf4j
 @RestControllerAdvice
@@ -21,5 +21,20 @@ public class CustomExceptionHandler {
                 .status(e.getCustomErrorCode())
                 .statusMessage(e.getStatusMessage())
                 .build(), e.getCustomErrorCode().getStatus());
+    }
+
+    @ExceptionHandler(HttpClientErrorException.class)
+    public ResponseEntity<Object> handleUnauthorizedException(
+            HttpClientErrorException e,
+            HttpServletRequest request) {
+
+        CustomException error = new CustomException(CustomErrorCode.USER_NOT_FOUND);
+
+        log.error("에러!! 요청 URL: {}, 에러 메시지: {}", request.getRequestURI(), error.getStatusMessage());
+
+        return new ResponseEntity<>(CustomErrorResponse.builder()
+                .status(error.getCustomErrorCode())
+                .statusMessage(error.getStatusMessage())
+                .build(), error.getCustomErrorCode().getStatus());
     }
 }
